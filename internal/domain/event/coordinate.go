91b2cd4357a83e846e9fdf9d7a9ab12918f2c505
BogdanStaziyev <94914224+BogdanStaziyev@users.noreply.github.com>
2022-06-19 16:30:48 +0300

    "fix"

--- internal/domain/event/coordinate.go
+++ internal/domain/event/coordinate.go
@@ -1,8 +1,10 @@
 package coordinate
 
 type Coordinate struct {
-	X1 		float64		`json:"X1" db:"X1"`
-	Y1		float64		`json:"Y1" db:"Y1"`
-	X2		float64		`json:"X2" db:"X2"`
-	Y2		float64		`json:"Y2" db:"Y2"`
+	Id      int     `json:"id" db:"id"`
+	MT      int     `json:"mt" db:"mt"`
+	Axis    string  `json:"axis" db:"axis"`
+	Horizon string  `json:"horizon" db:"horizon"`
+	X       float64 `json:"x" db:"x"`
+	Y       float64 `json:"y" db:"y"`
 }
