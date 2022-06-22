ef5cdb2d3a1ca3a0a175ab4633523d189ee852f4
BogdanStaziyev <94914224+BogdanStaziyev@users.noreply.github.com>
2022-06-22 20:40:50 +0300

    "validate"

--- go.mod
+++ go.mod
@@ -8,6 +8,9 @@ require (
 )
 
 require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.10.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -16,6 +19,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.9.1 // indirect
 	github.com/jackc/pgx/v4 v4.14.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b // indirect
 	golang.org/x/text v0.3.7 // indirect
--- internal/infra/http/controllers/coordinate_controller.go
+++ internal/infra/http/controllers/coordinate_controller.go
@@ -4,19 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/v5"
-	"log"
 	"net/http"
 	"startUp/internal/domain/coordinates"
+	"startUp/internal/infra/http/validators"
 	"strconv"
 )
 
 type EventController struct {
-	service *coordinate.Service
+	service 		*coordinate.Service
+	validator 		*validators.CoordinateValidator,
 }
 
 func NewEventController(s *coordinate.Service) *EventController {
 	return &EventController{
-		service: s,
+		service: 			s,
+		validator: 			validators.NewCoordinateValidator(),
 	}
 }
 
@@ -69,14 +71,6 @@ func (c *EventController) AddCoordinate() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		var coordinate coordinate.Coordinate
 		json.NewDecoder(request.Body).Decode(&coordinate)
-		if coordinate.X == 0 {
-			log.Fatal("X not exist")
-		} else if coordinate.Y == 0 {
-			log.Fatal("Y not exist")
-		} else if coordinate.MT == 0 {
-			fmt.Println("MT not exist")
-			return
-		}
 		err := (*c.service).AddCoordinate(coordinate)
 		if err != nil {
 			fmt.Printf("CoordinateController.AddCoordinate(): %s", err)
